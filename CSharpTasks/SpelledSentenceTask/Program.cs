511d939c2cca33aeced7c461b71fd33b78df8607
pebe <necromos@gmail.com>
2022-03-13 18:37:50 +0100

    Task initial

---
+++ CSharpTasks/SpelledSentenceTask/Program.cs
@@ -0,0 +1,13 @@
+﻿// + zadanie domowe zeby napisac metoda ktora przyjmuje zdanie i policzy wystapienia wszystkich
+// literek w tym zdaniu (w C#) zacznij od testow jednostkowych zeby bylo prosciej
+
+
+using CSharpTasks;
+
+Console.WriteLine("Hello! Please write your sentence to check how many times each letter is spelled.");
+
+Sentence sentence = new Sentence(Console.ReadLine());   
+List<string> spelled = new LetteringFactory().Create(sentence);
+foreach(string a in spelled){
+Console.WriteLine(a);
+}
---
+++ CSharpTasks/SpelledSentenceTask/Sentence.cs
@@ -0,0 +1,20 @@
+namespace CSharpTasks;
+
+public class Sentence
+{
+    private string sentence;
+    
+
+    public Sentence(string sentence)
+    {
+        this.sentence = sentence;
+    }
+
+    public string GetSentence()
+    {
+        return sentence;
+    }
+
+  
+    
+}
